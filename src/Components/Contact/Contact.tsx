584d669f39c112715ee676deb740e8ecccc4fb51
Eloise <eloise.cress@hotmail.co.uk>
2024-04-24 14:45:54 +0100

    addition of carousel and content

--- src/Components/Contact/Contact.tsx
+++ src/Components/Contact/Contact.tsx
@@ -1,7 +1,66 @@
-import "./Contact.css"
+import "./Contact.css";
+import "../../../globals.css";
+import * as React from "react";
 
-export default function Contact(){
-    return(
-        <h1>Contact</h1>
-    );
+import { Button } from "../ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "../ui/card";
+import { Input } from "../ui/input";
+import { Label } from "../ui/label";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "../ui/select";
+
+export default function Contact() {
+  return (
+    <>
+      <h1>Contact</h1>
+      <Card className="w-[350px]">
+        <CardHeader className="bg-blue-500">
+          <CardTitle>Contact Me</CardTitle>
+          <CardDescription>
+            Please fill out this form to send your details
+          </CardDescription>
+        </CardHeader>
+        <CardContent className="bg-blue-500">
+          <form>
+            <div className="grid w-full items-center gap-4">
+              <div className="flex flex-col space-y-1.5">
+                <Label htmlFor="name">Enter your name</Label>
+                <Input id="name" placeholder="Name" />
+              </div>
+              <div className="flex flex-col space-y-1.5">
+                <Label htmlFor="framework">Reason for contacting</Label>
+                <Select>
+                  <SelectTrigger id="framework">
+                    <SelectValue placeholder="Select" />
+                  </SelectTrigger>
+                  <SelectContent position="popper">
+                    <SelectItem value="next">Employer</SelectItem>
+                    <SelectItem value="sveltekit">Fellow coder</SelectItem>
+                    <SelectItem value="astro">Recruiter</SelectItem>
+                    <SelectItem value="nuxt">Just for fun!</SelectItem>
+                  </SelectContent>
+                </Select>
+              </div>
+            </div>
+          </form>
+        </CardContent>
+        <CardFooter className="flex justify-between bg-blue-500">
+          <Button variant="outline">Cancel</Button>
+          <Button className="bg-green-500">Submit</Button>
+        </CardFooter>
+      </Card>
+    </>
+  );
 }
--- src/Components/Projects/Projects.tsx
+++ src/Components/Projects/Projects.tsx
@@ -1,7 +1,41 @@
-import "./Projects.css"
+import "./Projects.css";
+import * as React from "react";
 
-export default function Projects(){
-    return(
-        <h1>Projects</h1>
-    );
+import { Card, CardContent } from "../ui/card";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "../ui/carousel";
+
+export default function Projects() {
+  return (
+    <>
+      <h1>Projects</h1>
+      <Carousel
+        opts={{
+          align: "start",
+        }}
+        className="w-full max-w-sm m-14"
+      >
+        <CarouselContent>
+          {Array.from({ length: 5 }).map((_, index) => (
+            <CarouselItem key={index} className="md:basis-1/2 lg:basis-1/3">
+              <div className="p-1">
+                <Card>
+                  <CardContent className="flex aspect-square items-center justify-center p-6">
+                    <span className="text-3xl font-semibold">{index + 1}</span>
+                  </CardContent>
+                </Card>
+              </div>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
+    </>
+  );
 }
