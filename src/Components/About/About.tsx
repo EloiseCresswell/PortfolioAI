e4826bfd18823609bd5d06b41baee9c1332abf1b
Eloise <eloise.cress@hotmail.co.uk>
2024-04-26 11:48:54 +0100

    addition of our learnings carousel

--- src/Components/About/About.tsx
+++ src/Components/About/About.tsx
@@ -1,5 +1,19 @@
 import "./About.css";
+import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
 
 export default function About() {
-  return <h1>About Us!</h1>;
+  return (
+    <>
+      <div className="popovers">
+        <Popover>
+          <PopoverTrigger className="border-solid border-2 border-black hover:border-dotted p-2 m-5">
+            Our Team
+          </PopoverTrigger>
+          <PopoverContent className="h-45 w-45">
+            <img src="/photos.png" className="imageOfUs" />
+          </PopoverContent>
+        </Popover>
+      </div>
+    </>
+  );
 }
--- src/Components/Projects/Projects.tsx
+++ src/Components/Projects/Projects.tsx
@@ -13,38 +13,41 @@ import {
 export default function Projects() {
   let trial: string[] = [
     "/eanda.png",
-    "/photos.png",
-    "may",
-    "/me.png",
-    "/vite.svg",
+    "/r.png",
+    "/c.png",
+    "/cv.png",
+    "/cv2.png",
+    "/cv3.png",
   ];
   return (
     <>
       <h1>Our Work</h1>
-      <Carousel
-        opts={{
-          align: "start",
-        }}
-        className="w-100 h-100 m-14"
-      >
-        <CarouselContent>
-          {trial.map((element, index) => (
-            <CarouselItem key={index} className="md:basis-1/1 lg:basis-1/3">
-              <div className="p-1">
-                <Card>
-                  <CardContent className="flex aspect-square items-center justify-center p-6">
-                    <span className="text-3xl font-semibold">
-                      <img src={element} />
-                    </span>
-                  </CardContent>
-                </Card>
-              </div>
-            </CarouselItem>
-          ))}
-        </CarouselContent>
-        <CarouselPrevious />
-        <CarouselNext />
-      </Carousel>
+      <div className="carouselDiv">
+        <Carousel
+          opts={{
+            align: "start",
+          }}
+          className="w-full h-fit m-5"
+        >
+          <CarouselContent>
+            {trial.map((element, index) => (
+              <CarouselItem key={index} className="md:basis-1/1 lg:basis-1/1">
+                <div className="p-1">
+                  <Card>
+                    <CardContent className="flex items-center justify-center align-top p-6">
+                      <span className="text-3xl font-semibold ">
+                        <img src={element} className="workImages" />
+                      </span>
+                    </CardContent>
+                  </Card>
+                </div>
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+          <CarouselPrevious />
+          <CarouselNext />
+        </Carousel>
+      </div>
     </>
   );
 }
